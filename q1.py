ea32e72d6aef524db7f2c572671273c32507fdda
JesseSinghGIT <123704475+JesseSinghGIT@users.noreply.github.com>
2023-03-07 10:03:23 +0000

    program

--- q1.py
+++ q1.py
@@ -1,6 +1,30 @@
 
-# ******************************
+# *****************************
 # Make your Code
+
+
+
+#2
+flag = 0 
+counter = 0  
+iterations = 10
+iseven = False
+
+for i in range(iterations):
+    usernum = int(input('Enter a number: '))
+    if (usernum % 2 == 0 ):
+        iseven = True
+    else: 
+        iseven = False
+    if (flag == 1 and iseven == True):
+        counter += 1 
+    if (iseven == True): 
+        flag += 1 
+    else:
+        flag = 0
+print (counter)
+
+    
 # ******************************
 
 
